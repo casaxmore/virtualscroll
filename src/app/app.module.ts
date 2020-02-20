bb5387004599a2debf560f26df6bd48f36e111be
jmcasabona <casaxmore@gmail.com>
2020-02-20 14:19:43 +0100

    Lista ordenada cargada desde un servicio res e incluido drag para moverlo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// Virutal Scroll
+// Virtual Scroll
 import { ScrollingModule } from '@angular/cdk/scrolling';
 
 // Drag
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
+// Http para acceder y consumir peticiones res
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './virtual/virtual.component';
 import { DragComponent } from './drag/drag.component';
+import { PaisesComponent } from './paises/paises.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VirtualComponent,
-    DragComponent
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule,
     ScrollingModule,
-    DragDropModule
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
